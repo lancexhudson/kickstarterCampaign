567d46becdde0afda7ecf3260b0b4cdcd9e15798
Lance Hudson <lancexhudson@gmail.com>
2022-11-25 11:30:38 -0500

    errorMessage and Buttons

--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -6,11 +6,12 @@ import { Link, Router } from '../../../routes';
 import Layout from '../../../components/Layout';
 
 class RequestNew extends Component {
-
 state = {
     value: '',
     description: '',
-    recipient: ''
+    recipient: '',
+    loading: false,
+    errorMessage: ''
 };
 
 static async getInitialProps(props) {
@@ -23,22 +24,35 @@ onSubmit = async event => {
 
     const campaign = Campaign(this.props.address);
     const { description, value, recipient } = this.state;
+    this.setState({ loading: true, errorMessage: '' })
 
     try {
         const accounts = await web3.eth.getAccounts();
         await campaign.methods.createRequest(
             description, 
             web3.utils.toWei(value, 'ether'),
-            recipient
-        ).send({ from: accounts[0] });
-    } catch (err) {}
+            recipient)
+            .send({ from: accounts[0] });
+
+            Router.pushRoute(`/campaigns/${this.props.address}/requests`);
+    } catch (err) {
+        this.setState({ errorMessage: err.message });
+    }
+
+    this.setState({ loading: false });
 };
 
     render() {
         return (
             <Layout>
+
+                <Link route={`/campaigns/${this.props.address}/requests`}>
+                <a>Back</a>
+                </Link>
+                
                 <h3>Create a Request</h3>
-        <Form onSubmit={this.onSubmit}>
+                
+        <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
 
             <Form.Field>
                 <label>Description</label>
@@ -66,7 +80,8 @@ onSubmit = async event => {
                 />
             </Form.Field>
 
-            <Button primary>Create!</Button>
+                    <Message error header="Oops!" content={this.state.errorMessage} />
+            <Button primary loading={this.state.loading}>Create!</Button>
             {/* importing Layout helps with styling -- primary  */}
 
         </Form>
